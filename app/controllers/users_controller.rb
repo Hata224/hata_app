fcf355ab1859cba7b7a61f7ad758801b91a4f843
Hata <kh6305su@gmail.com>
2019-11-19 14:02:21 +0900

    [change]自分以外のユーザー情報を編集できないように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -26,16 +28,23 @@ end
    def update
      @user = User.find(params[:id])
      if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = "プロフィールを更新しました"
       redirect_to @user
      else
-     render 'edit'
+      render 'edit'
      end
    end
 
   private
 
   def user_params
-     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+   params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
+
+  def logged_in_user
+    unless logged_in?
+    flash[:danger] = "ログインしてください"
+    redirect_to login_url
     end
+  end
 end
