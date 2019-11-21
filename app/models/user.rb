de207b8de99b76d74e054cd7df473674bebbd967
Hata <kh6305su@gmail.com>
2019-11-21 22:14:17 +0900

    [add]Userモデルで使うモジュールを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
   attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
